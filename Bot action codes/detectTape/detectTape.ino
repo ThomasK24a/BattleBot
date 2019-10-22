84a5990a5f8de882f59874cdaae8b519d0e5d509
Thomas Koops <thomas_koops@hotmail.com>
2019-10-22 11:59:27 +0200

    Rename stuff

---
+++ Bot action codes/detectTape/detectTape.ino
@@ -0,0 +1,15 @@
+int sensorPinLeft = 10;
+int sensorPinRight = 11;
+int sensorValueLeft;
+
+void setup() 
+{
+  Serial.begin(9600); //sets serial port for communication
+  
+}
+
+void loop() 
+{
+  sensorValueLeft = digitalRead(sensorPinLeft); // read the value from the sensor
+  Serial.println(sensorValueLeft == 1); //prints the values coming from the sensor on the screen
+}
